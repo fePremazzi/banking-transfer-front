e4dc3ec4079a8050812549d3b004298f7f2ba2d2
Fellipe Rego <felliperego@hotmail.com>
2020-07-23 00:43:46 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SchedulerBankingTransfer';
+  title = 'Banking transfer scheduler';
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+  createForm() {
+    this.angForm = this.fb.group({
+      originAccount: ['', Validators.required],
+      destinationAccount: ['', Validators.required]
+    });
+  }
 }
