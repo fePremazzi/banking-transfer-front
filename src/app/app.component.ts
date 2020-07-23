5065726bd830304d9c40bbaba8fe48cb0cf631d3
Fellipe Rego <felliperego@hotmail.com>
2020-07-23 02:05:08 -0300

    delete form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,22 @@ import { CustomValidators } from 'ngx-custom-validators';
 export class AppComponent {
   title = 'Banking transfer scheduler';
   angForm: FormGroup;
+  angDeleteForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.createForm();
+    this.createDeleteForm();
   }
   createForm() {
     this.angForm = this.fb.group({
       originAccount: ['', Validators.required],
       destinationAccount: ['', Validators.required],
       transferValue: ['', [Validators.pattern("^[0-9]*.[0-9]*$"), Validators.min(0.01)]],
-      //scheduledDate: ['', Validators.pattern("^(19|20)\d\d[/](0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])$")]
       scheduledDate: ['',[ CustomValidators.date, Validators.required]]
+    });
+  }
+  createDeleteForm() {
+    this.angDeleteForm = this.fb.group({
+      transferId: ['', [Validators.pattern("^[0-9]*$"), Validators.min(1)]]
     });
   }
 }
