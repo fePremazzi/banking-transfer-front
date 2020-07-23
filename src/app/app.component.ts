64caa50e163a0f42552637bdda734ec025a190e6
Fellipe Rego <felliperego@hotmail.com>
2020-07-23 01:43:31 -0300

    ngx-custom-validators for date validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { FormGroup,  FormBuilder,  Validators} from '@angular/forms';
+import { CustomValidators } from 'ngx-custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,11 @@ export class AppComponent {
   createForm() {
     this.angForm = this.fb.group({
       originAccount: ['', Validators.required],
-      destinationAccount: ['', Validators.required]
+      destinationAccount: ['', Validators.required],
+      transferValue: ['', [Validators.pattern("^[0-9]*.[0-9]*$"), Validators.min(0.01)]],
+      //scheduledDate: ['', Validators.pattern("^(19|20)\d\d[/](0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])$")]
+      scheduledDate: ['',[ CustomValidators.date, Validators.required]]
     });
   }
 }
+//2020/05/05
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule  } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
